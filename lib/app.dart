edd0d472edceb867e1c5b4eeceec43a963295989
tharindu97 <tharindukavishna@gmail.com>
2019-12-10 16:02:59 +0530

    start_ui_step1

---
+++ lib/app.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class LandingScreen extends StatefulWidget {
+  LandingScreen({Key key}) : super(key: key);
+
+  @override
+  _LandingScreenState createState() => _LandingScreenState();
+}
+
+class _LandingScreenState extends State<LandingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Main Screen"),
+      ),
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Text("No Image Selected"),
+              RaisedButton( onPressed: (){
+
+              }, child: Text("Select Image"),)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
